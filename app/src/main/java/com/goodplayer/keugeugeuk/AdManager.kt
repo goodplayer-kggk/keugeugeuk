37eb5673b6aae43a437c5bd53dcfeeaca62c979e
goodplayer-kggk <keugeugeuk@gmail.com>
2025-08-12 20:19:31 +0900

    add admanager object
    Signed-off-by: goodplayer-kggk <keugeugeuk@gmail.com>

---
+++ app/src/main/java/com/goodplayer/keugeugeuk/AdManager.kt
@@ -0,0 +1,11 @@
+package com.goodplayer.keugeugeuk
+
+import kotlinx.coroutines.delay
+
+object AdManager {
+    // simulated ad watch: waits 2s and returns true (watched)
+    suspend fun simulateWatch(): Boolean {
+        delay(1200) // simulate ad short load + watch
+        return true
+    }
+}
